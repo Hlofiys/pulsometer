02108fa1d2eaccfc4e12cf1572a6c262c137031c
Tima-Zan <tzaneuski@gmail.com>
2024-11-22 14:58:51 +0300

    Added https in swagger

--- backend/src/main/java/ru/zan/Pulsometer/config/SwaggerConfig.java
+++ backend/src/main/java/ru/zan/Pulsometer/config/SwaggerConfig.java
@@ -2,14 +2,23 @@ package ru.zan.Pulsometer.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+    @Value("${base_url}")
+    String baseUrl;
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .addServersItem(new Server()
+                        .url(baseUrl)
+                )
                 .info(
                         new Info()
                                 .title("Pulsometer")
